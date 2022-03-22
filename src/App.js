a2421e38148d5f6aff2e58ccd9a075c7788a9eb4
FadellMuhammad <fadel54031@gmail.com>
2022-03-22 22:37:47 +0700

    add data.js and show in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './Data';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={data.album.images[1].url} alt="" />
+      <p>Song title : {data.name}</p>
+      <p>Song Artists : {data.artists[0].name}</p>
+      <button type='submit'>Select</button>
     </div>
   );
 }
